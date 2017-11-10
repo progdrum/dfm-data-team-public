6aebfb998b2a90d7c886c2149d1ea6ec99578477
progdrum <brookforestscitech@gmail.com>
2017-11-09 22:26:03 -0700

    Initial setup

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'postgres://sean:weakpassword@localhost:5432/testdb'
 
 
 @app.route("/")
 def output():
+    """
+    Brings up page to parse the data, insert into the database 
+    and redirect to the next page, displaying the result.
+    
+    :return: 
+    """
     return "Output results here."
+
 
 if __name__ == "__main__":
     app.run(debug=True)
